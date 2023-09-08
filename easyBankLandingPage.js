92cd072a81ae436ea416a6f1e94e1feb5390335b
Daniel Oluwayomi <oluwayomid00@gmail.com>
2023-09-08 14:10:54 +0100

    made it more interactive

--- easyBankLandingPage.js
+++ easyBankLandingPage.js
@@ -1,7 +1,10 @@
 $('.hamburger').click(function () { 
     $('header div ul').css('top', '0');
+    // $('header div ul').animate({top: '0'});
+    
 });
 $('.close').click(function () { 
+    // $('header div ul').animate({top: '-100%'}, '0.7s');
     $('header div ul').css('top', '-100%');
     // $('header div ul').css('position', 'relative');
 });
